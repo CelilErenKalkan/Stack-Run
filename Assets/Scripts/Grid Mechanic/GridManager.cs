f6141b9ba282edb951f4bc41c60bba85024e3d2c
Celil Eren Kalkan <celilerenkalkan@hotmail.com>
2025-07-07 02:11:26 +0300

    Update GridManager.cs

--- Assets/Scripts/Grid Mechanic/GridManager.cs
+++ Assets/Scripts/Grid Mechanic/GridManager.cs
@@ -152,7 +152,7 @@ namespace Grid_Mechanic
         public static float FinishLineDistanceCalculation()
         {
             var multiplier = DataManager.GetLevel / 10;
-            return (multiplier * 10 + DataManager.GetLevel + 50.0f) % 100;
+            return (multiplier * 10 + DataManager.GetLevel + 10.0f) % 100;
         }
 
         public static void SpawnFinishLine(float distanceZ)
